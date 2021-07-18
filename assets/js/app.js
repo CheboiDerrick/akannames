843e8aba4e28ab04844c600a3c2f8ae6b61657e0
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-07-19 01:11:42 +0300

    add arrays for male female names

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,6 @@ let gender;
 let dateInput;
 let userDate;
 
+//Array storing names for both male and female akan names
+const akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
