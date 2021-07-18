6f92334aced91b2f379ec24e39ab635783ab8cbc
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-07-19 01:13:34 +0300

    add function to get user input

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,13 @@ let userDate;
 //Array storing names for both male and female akan names
 const akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+//Function to get the input from the user
+let getInputs=() =>{
+    gender=document.getElementById("gender").value;
+    console.log(gender);
+    dateInput= document.getElementById('date').value;
+    console.log(dateInput);
+    userDate= new Date(dateInput);
+    console.log(userDate);
+}
