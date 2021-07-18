6c14d08841b51554f2ab2d2629d01e524b525f5d
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-07-19 01:46:21 +0300

    add function prevent default

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,17 +12,17 @@ const akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 const akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 //Function to get the input from the user
-let getInputs=() =>{
-    gender=document.getElementById("gender").value;
+let getInputs = () => {
+    gender = document.getElementById("gender").value;
     console.log(gender);
-    dateInput= document.getElementById('date').value;
+    dateInput = document.getElementById('date').value;
     console.log(dateInput);
-    userDate= new Date(dateInput);
+    userDate = new Date(dateInput);
     console.log(userDate);
 }
 
 //Function to get the day of the week d
-let getDay=() =>{
+let getDay = () => {
     getInputs();
     console.log(userDate);
     CC = userDate.getFullYear();
@@ -39,38 +39,57 @@ let getDay=() =>{
     d = userDate.getDay();
     console.log(d);
     day = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
-    day=Math.round(day);
+    day = Math.round(day);
     return d;
 }
 
 //function to assign the aka name based on the value d returned
-let assignAkan=() =>{
+let assignAkan = () => {
     getDay();
-    if (gender=="Female") {
-        akanName=akanFemale[d];
-        messageDisplay='Your Akan Name is '+ <strong>+<em>+akanName+ </em>+</strong>+ ', meaning female born on  '+ userDate.toLocaleString('en-us', { weekday: 'long' })
+    if (gender == "Female") {
+        akanName = akanFemale[d];
+        messageDisplay = 'Your Akan Name is ' + '<strong>' + '<em>' + akanName + '</em>' + '</strong>' + ', meaning female born on  ' + userDate.toLocaleString('en-us', { weekday: 'long' })
         console.log(messageDisplay);
         document.getElementById("display").innerHTML = messageDisplay;
     }
     else {
-        akanName=akanMale[d];
-        messageDisplay='Your Akan Name is '+ akanName+ ', meaning a male born on  '+ userDate.toLocaleString('en-us', { weekday: 'long' });
+        akanName = akanMale[d];
+        messageDisplay = 'Your Akan Name is ' + akanName + ', meaning a male born on  ' + userDate.toLocaleString('en-us', { weekday: 'long' });
         console.log(messageDisplay);
         document.getElementById("display").innerHTML = messageDisplay;
     }
 }
 
 //function to validate the user input and initialize whole program
-let validateInput= () => {
+let validateInput = () => {
     getInputs();
-    if (userDate>new Date()||isNaN(userDate)){
+    if (userDate > new Date() || isNaN(userDate)) {
         alert(`Enter a correct date`);
     }
-    else if (gender=='default') {
+    else if (gender == 'default') {
         alert('Please select your gender');
     }
     else {
         assignAkan();
     }
 }
+
+//function to prevent automatic page reload
+document.getElementById("buttonReveal").addEventListener("click", function (event) {
+    event.preventDefault();
+    validateInput();
+});
+
+// l
+//     getInputs();
+//     if (userDate>new Date()||isNaN(userDate)){
+//         alert(`Enter a correct date`);
+//     }
+//     else if (gender=='default') {
+//         alert('Please select your gender');
+//     }
+//     else {
+//         assignAkan();
+//     }
+// }
 
